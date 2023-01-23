73d41a614ce82cbd510fc3a513cb60b32c5f8912
Krish Patel <patel.12102325@lpu.in>
2023-01-23 19:49:03 +0530

    Create 12 Total_Order_SumProblem.java
    int Total = N+X;

---
+++ 12 Total_Order_SumProblem.java
@@ -0,0 +1,43 @@
+/*
+Consider that two friends Neha and Nisha are going for shopping .Neha has picked the N number of Items from the Market and Nisha has picked X number of Items from the Market .Find out total numbers of Items ordered by both .
+
+Input Format
+
+In First input line, you should enter number of Items ordered by Neha.
+In Second input line, you should enter number of Items ordered by Nisha.
+
+Constraints
+
+Enter only integer (positive) value.
+0<=n<=100
+
+Output Format
+
+Total numbers of items ordered by Neha and Nisha during shopping.
+
+Sample Input 0
+
+5
+10
+Sample Output 0
+
+15
+*/
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc= new Scanner(System.in);
+        //Neha's Order
+        int N = sc.nextInt();
+        //Nisha's Order
+        int X  = sc.nextInt();
+        //Total Order
+        int Total = N+X;
+        //Result
+        System.out.print(Total);
+    }
+}
